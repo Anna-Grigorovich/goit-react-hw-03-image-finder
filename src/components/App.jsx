b9da7cb4b8333c78382341a8e636a28c1102f796
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-05-19 01:32:47 +0300

    hm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import getImages from '../services/Api';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Modal } from './Modal/Modal';
+import { Loader } from './Loader/Loader';
+import c from './Loader/Loader.module.css';
+import { Button } from './Button/Button';
+
+export class App extends Component {
+  state = {
+    input: '',
+    query: [],
+    error: null,
+    status: 'idle',
+    showModal: false,
+    largeImg: '',
+    page: 1,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextQuery = this.state.input;
+    const prevQuery = prevState.input;
+    const nextPage = this.state.page;
+    const prevPage = prevState.page;
+
+    if (prevQuery !== nextQuery || prevPage !== nextPage) {
+      this.setState({ status: 'pending' });
+      getImages(nextQuery, nextPage)
+        .then(({ hits }) =>
+          this.setState({
+            query: [...prevState.query, ...hits],
+            status: 'resolved',
+          })
+        )
+        .catch(error => this.setState({ error, status: 'rejected' }));
+    }
+  }
+  formSubmit = input => {
+    this.setState({ input: input.toLowerCase() });
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+  handleClickImg = e => {
+    const large = e.currentTarget.dataset.src;
+    this.setState({ largeImg: large, showModal: true });
+  };
+  handleClickLoad = () => {
+    this.setState(({ page }) => ({
+      page: page + 1,
+    }));
+  };
+
+  render() {
+    const { showModal, query, largeImg, status, error } = this.state;
+    return (
+      <>
+        <ToastContainer />
+        {status === 'rejected' && <h1>{error.message}</h1>}
+        <Searchbar formSubmit={this.formSubmit} />
+        <ImageGallery query={query} onClickImg={this.handleClickImg} />
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <img src={largeImg} alt="" />
+          </Modal>
+        )}
+        {status === 'pending' && <Loader />}
+        {query.length >= 12 && <Button onClick={this.handleClickLoad} />}
+      </>
+    );
+  }
+}
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+import c from './Modal.module.css';
+import PropTypes from 'prop-types';
+
+export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleEsc);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleEsc);
+  }
+  handleEsc = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+  render() {
+    return (
+      <div className={c.overlay} onClick={this.handleBackdropClick}>
+        <div className={c.modal}>{this.props.children}</div>
+      </div>
+    );
+  }
+}
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
